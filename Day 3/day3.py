e6371e3513b9a3ff98a70c017b935be9e4f4ffed
parasgadal67-oct <gadalparas67@gmail.com>
2026-04-26 12:45:48 +0530

    Create day3.py
    create a simple calculator

---
+++ Day 3/day3.py
@@ -0,0 +1,29 @@
+# creating simple calculator
+#addition
+x = float(input("What is x: "))
+y = float(input("What is y: "))
+z = x + y
+
+print(" The sum of x and y is:",z)
+
+#subtraction
+x = float(input("What is x: "))
+y = float(input("What is y: "))
+z = x - y
+
+print("The difference of x and y is: ",z)
+
+#multiplication
+x = float(input("What is x: "))
+y = float(input("What is y: "))
+z = x * y
+
+print("The product of x and y is: ",z)
+
+#division
+x = float(input("What is x: "))
+y = float(input("What is y: "))
+z = x / y
+a = x % y
+print("The quotient of x and y is: ",z)
+print("The remainder of x and y is: ",a)
