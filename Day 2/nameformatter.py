bd30c9da39cd9cb3113591bc2b6bd672a071f93c
parasgadal67-oct <gadalparas67@gmail.com>
2026-04-27 17:47:05 +0530

    Day 2 mini projects

---
+++ Day 2/nameformatter.py
@@ -0,0 +1,11 @@
+# take person name and city
+#convert name to lowercase + remove space
+# combine name + city length as username
+
+username = "Paras Gadal"
+city = "dehradun"
+
+print(len(city))
+print(username.lower().replace(" ","") + str(len(city)))
+
+
