89b8bdd2dcfea7340e1e34272efd76c00a77861d
parasgadal67-oct <gadalparas67@gmail.com>
2026-04-30 21:23:06 +0530

    Day 7 mini projects

---
+++ Day 7/fruits.py
@@ -0,0 +1,19 @@
+#creating fruit lists
+fruits = ["Apple" , "Banana" , "Orange" , "Mango" ,"Kiwi"]
+fruit = fruits.append("Guava")
+fruit = fruits.remove("Apple")
+
+print(fruits)
+
+#finding the index of the list
+print(fruits.index("Orange"))
+
+# adding the new fruit to list with .append()
+print(fruits)
+
+#showing that we can remove the item from the list
+print(fruits)
+
+#finding length and counting of list
+print(len(fruits))
+print(fruits.count("Mango"))
---
+++ Day 7/shoppinglist.py
@@ -0,0 +1,7 @@
+#creating shopping list
+cart_item = ["bags" , "shoes" , "shirts" , "pants" ,] 
+cart_item.append("trousers")
+cart_item.remove("bags")
+
+print(cart_item[0])
+ 
